24344d88c049e2dbd7c0053a7baac2c42df61ae2
Kamal Kumar <109294674+kamal-telus@users.noreply.github.com>
2024-09-20 01:38:14 +0530

    modified test case

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Apps/i);
+  const linkElement = screen.getByText(/App/i);
   expect(linkElement).toBeInTheDocument();
 });
