c1e957e5374e2f2f43a36082ba0449e3b420ad78
Kamal Kumar <109294674+kamal-telus@users.noreply.github.com>
2024-09-29 01:23:37 +0530

    add todo component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Welcome';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import Home from './components/Index';
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
-      <header className="App-header">
-          <Welcome name="Sara" />
-          <img src={logo} className="App-logo" alt="logo" />
-      </header>
+          <header className="App-header">
+            
+            <BrowserRouter>
+            <div className="menu">
+              <NavLink to="/" className={'navbar'}> <span > Home </span> </NavLink>
+              <NavLink to="/welcome" className={'navbar'}> <span > Welcome </span> </NavLink>
+              <NavLink to="/welcome" className={'navbar'}> <span > Welcome3 </span> </NavLink>
+              <NavLink to="/welcome" className={'navbar'}> <span > Welcome4 </span> </NavLink>
+              </div>
+              <div className=''>
+                <Routes>
+                  <Route path="/welcome" element={<Welcome />} />
+                  <Route path="/" element={<Home />} />
+                </Routes>
+              </div>
+              
+            </BrowserRouter>
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
     </div>
   );
 }
